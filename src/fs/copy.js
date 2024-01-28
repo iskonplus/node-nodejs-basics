80353e4cf92a608c1e3a4382cde41e32e14f4aaa
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-01-28 15:48:48 +0100

    feat: add solution copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,10 @@
+import fs from 'fs';
+const errorCopy = new TypeError('FS operation failed');
+
 const copy = async () => {
-    // Write your code here 
+    fs.cp('./src/fs/files', './src/fs/files_copy', { recursive: true, force: false, errorOnExist: true }, err => {
+        err && console.error(errorCopy.message);
+    });
 };
 
 await copy();
