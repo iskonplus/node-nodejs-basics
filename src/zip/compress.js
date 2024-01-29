d848bf120eea0cd0e0a5893bd18eb3e3980578c7
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-01-29 16:16:29 +0100

    feat: add solution compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readFileStream = fs.createReadStream('./src/zip/files/fileToCompress.txt');
+    const writeFileStream = fs.createWriteStream('./src/zip/files/archive.gz');
+
+    readFileStream.pipe(createGzip()).pipe(writeFileStream);
+
 };
 
 await compress();
