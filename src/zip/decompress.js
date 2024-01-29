332a877486a020c4781f21cdef0992eabc848705
iskonplus <58971909+iskonplus@users.noreply.github.com>
2024-01-29 16:25:25 +0100

    feat: add solution decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,11 @@
+import fs from 'fs';
+import { createGunzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const readFileStream = fs.createReadStream('./src/zip/files/archive.gz');
+    const writeFileStream = fs.createWriteStream('./src/zip/files/fileToCompress.txt');
+
+    readFileStream.pipe(createGunzip()).pipe(writeFileStream);
 };
 
 await decompress();
