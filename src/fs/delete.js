923cde581e11715a0cfcff0d83a9c40825e6de47
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-21 21:23:09 +0200

    feat: implement delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { unlink } from 'fs/promises';
+
+const error = new TypeError('FS operation failed');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToRemove.txt')
+
 const remove = async () => {
-  // Write your code here
+  unlink(filePath)
+    .catch(err => {
+      err && console.error(error.message);
+    })
 };
 
 await remove();
