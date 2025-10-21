cdf706b91ca6f1a6dc48121f1908ec64dd4cb8f7
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-21 22:34:33 +0200

    feat: implement env.js task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
 const parseEnv = () => {
-  // Write your code here
+
+  const rssVars = process.env;
+  const result = [];
+
+  for (const key in rssVars) {
+    if (key.startsWith('RSS_')) {
+      result.push(`${key}=${rssVars[key]}`);
+    }
+  }
+
+  console.log(result.join('; '));
 };
 
 parseEnv();
