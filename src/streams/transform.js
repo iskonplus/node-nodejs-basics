d140bb7780afd93d4beb85d0277014d4941dd57a
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 17:10:05 +0200

    feat: implement transform.js task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-  // Write your code here
+
+  const transformStream = new Transform({
+    transform(chunk, enc, cb) {
+      cb(null, chunk.toString().split('').reverse().join(''));
+    }
+  });
+
+  process.stdin
+    .on('error', err => console.error(err))
+    .pipe(transformStream)
+    .pipe(process.stdout)
+
 };
 
 await transform();
