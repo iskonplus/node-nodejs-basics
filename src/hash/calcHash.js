bd854a19479e299562587834fbd346410a1ee969
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 15:50:11 +0200

    feat: implement calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-  // Write your code here
+  readFile(filePath, 'utf8')
+    .then(data => console.log(createHash('sha256').update(data).digest('hex')))
+    .catch(err => console.log(err));
 };
 
 await calculateHash();
