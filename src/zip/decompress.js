98622229807b0781e441530e9e1a9276466846bb
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 17:39:57 +0200

    feat: implement decompress.js task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'archive.gz');
+const unzipFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+const uncompress = createGunzip();
+
+
 const decompress = async () => {
-  // Write your code here
+
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(unzipFilePath);
+
+  readStream
+    .pipe(uncompress)
+    .pipe(writeStream)
+    .on('error', err => console.error(err))
+
 };
 
 await decompress();
