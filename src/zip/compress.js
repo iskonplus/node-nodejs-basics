34462d1ce851ea3da061b88d23b12a69b759db82
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 17:27:01 +0200

    feat: implement compress.js task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archiveFilePath = path.join(__dirname, 'files', 'archive.gz');
+
+const compressFile = createGzip();
+
 const compress = async () => {
-  // Write your code here
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(archiveFilePath);
+
+  readStream
+    .pipe(compressFile)
+    .pipe(writeStream)
+    .on('error', err => console.error(err))
+
 };
 
 await compress();
