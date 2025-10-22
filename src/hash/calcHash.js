cbfe7c869fc200e9c58dcbcf9dff4b7d4b5a0b47
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 16:01:46 +0200

    fix: refactor calcHash to use Streams API instead of readFile

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import { readFile } from 'fs/promises';
+import { createReadStream } from 'fs';
 import { createHash } from 'crypto';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,9 +10,14 @@ const __dirname = dirname(__filename);
 const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
 
 const calculateHash = async () => {
-  readFile(filePath, 'utf8')
-    .then(data => console.log(createHash('sha256').update(data).digest('hex')))
-    .catch(err => console.log(err));
+  
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', chunk => hash.update(chunk));
+  stream.on('end', _ => console.log(hash.digest('hex')));
+  stream.on('error', err => console.error(err));
+
 };
 
 await calculateHash();
