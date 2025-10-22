b24a24107f5d6bb66a79bbea0c6411a9c5d83390
Artur Tamashevich <TAMASHEVICH.ARTUR@GMAIL.COM>
2025-10-22 16:35:47 +0200

    feat: implement write file using stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-  // Write your code here
+
+  const writeStream = createWriteStream(filePath);
+
+  writeStream.on('error', err => console.error(err));
+  process.stdin.pipe(writeStream);
+
 };
 
 await write();
